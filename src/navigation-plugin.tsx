4c0be300ccce07d1c1d5306af4573eec5a71b8a2
Eitan Avgil <eitan.avgil@kaltura.com>
2020-06-18 11:07:11 +0300

    feat(FEV-542): topbar icon and basic kitchensink interaction

--- src/navigation-plugin.tsx
+++ src/navigation-plugin.tsx
@@ -5,13 +5,20 @@ import {
   OnMediaLoad,
   OnMediaUnload,
   OnPluginSetup,
-
   ContribServices,
   ContribPluginData,
   ContribPluginConfigs
 } from "@playkit-js-contrib/plugin";
 import { getContribLogger } from "@playkit-js-contrib/common";
-import * as classes from './navigation-plugin.scss';
+import * as styles from "./navigation-plugin.scss";
+import {
+  KitchenSinkContentRendererProps,
+  KitchenSinkExpandModes,
+  KitchenSinkItem,
+  KitchenSinkPositions,
+  UpperBarItem
+} from "@playkit-js-contrib/ui";
+import { getConfigValue } from "./utils/utils";
 
 const pluginName = `navigation`;
 
@@ -21,37 +28,83 @@ const logger = getContribLogger({
 });
 
 interface NavigationPluginConfig {
+  expandOnFirstPlay: boolean;
+  position: KitchenSinkPositions;
 }
 
-export class NavigationPlugin implements OnMediaLoad, OnMediaUnload, OnPluginSetup, OnMediaUnload {
+export class NavigationPlugin
+  implements OnMediaLoad, OnMediaUnload, OnPluginSetup, OnMediaUnload {
+  private _kitchenSinkItem: KitchenSinkItem | null = null;
+  private _upperBarItem: UpperBarItem | null = null;
 
   constructor(
     private _corePlugin: CorePlugin,
     private _contribServices: ContribServices,
     private _configs: ContribPluginConfigs<NavigationPluginConfig>
-  ) {
-  }
+  ) {}
 
   onPluginSetup(): void {
+    this._initKitchensinkAndUpperBarItems();
   }
 
-  onMediaLoad(): void {
-  }
+  onMediaLoad(): void {}
 
-  onMediaUnload(): void {
-  }
+  onMediaUnload(): void {}
 
-  onPluginDestroy(): void {
+  onPluginDestroy(): void {}
+
+  private _initKitchensinkAndUpperBarItems(): void {
+    if (!this._upperBarItem && !this._kitchenSinkItem) {
+      this._addKitchenSinkItem();
+    }
+  }
+  private _renderKitchenSinkContent = (
+    props: KitchenSinkContentRendererProps
+  ) => {
+    return <div>DDDDDDDFDFDF</div>;
+  };
+
+  private _addKitchenSinkItem(): void {
+    const { position, expandOnFirstPlay } = this._configs.pluginConfig;
+    // if (this._kitchenSinkItem) {
+    //   this._contribServices.upperBarManager.remove(this._kitchenSinkItem);
+    // }
+    this._kitchenSinkItem = this._contribServices.kitchenSinkManager.add({
+      label: "Navigation",
+      expandMode: KitchenSinkExpandModes.AlongSideTheVideo,
+      renderIcon: () => (
+        <div className={styles.pluginIcon} role="button" tabIndex={1} />
+      ),
+      position: getConfigValue(
+        position,
+        position =>
+          typeof position === "string" &&
+          (position === KitchenSinkPositions.Bottom ||
+            position === KitchenSinkPositions.Right),
+        KitchenSinkPositions.Bottom
+      ),
+      renderContent: this._renderKitchenSinkContent
+    });
+
+    if (expandOnFirstPlay) {
+      this._kitchenSinkItem.activate();
+    }
   }
 }
 
 ContribPluginManager.registerPlugin(
   pluginName,
   (data: ContribPluginData<NavigationPluginConfig>) => {
-    return new NavigationPlugin(data.corePlugin, data.contribServices, data.configs);
+    return new NavigationPlugin(
+      data.corePlugin,
+      data.contribServices,
+      data.configs
+    );
   },
   {
     defaultConfig: {
+      expandOnFirstPlay: false,
+      position: KitchenSinkPositions.Right
     }
   }
 );
