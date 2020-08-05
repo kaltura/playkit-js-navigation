b6476b8a30bade53e51f227fcf81f6c9e0a9a357
Serhii Marchenko <serhii.marchenko@Kaltura.com>
2020-08-05 17:04:15 +0300

    chore(ui): add i18n to Navigation-filter component (FEV-637)

--- src/components/navigation-filter/navigation-filter.tsx
+++ src/components/navigation-filter/navigation-filter.tsx
@@ -7,24 +7,26 @@ import {
   BackgroundColors,
 } from '../navigation/icons/IconsFactory';
 const {Tooltip} = KalturaPlayer.ui.components.Tooltip;
+const {Text} = KalturaPlayer.ui.preacti18n;
 
 export interface FilterProps {
   onChange(value: itemTypes): void;
   activeTab: itemTypes;
   availableTabs: itemTypes[];
   totalResults: number | null;
-  translates: Record<string, string>;
+  defaultTranslates: Record<string, string>;
 }
 
 export interface TabData {
   type: itemTypes;
   isActive: boolean;
   label: string;
+  i18nLabel: string;
 }
 
 export class NavigationFilter extends Component<FilterProps> {
   static defaultProps = {
-    translates: {
+    defaultTranslates: {
       [itemTypes.All]: 'All',
       [itemTypes.AnswerOnAir]: 'Answer On Air',
       [itemTypes.Chapter]: 'Chapters',
@@ -49,11 +51,7 @@ export class NavigationFilter extends Component<FilterProps> {
     this.props.onChange(type);
   };
 
-  public _renderTab = (tab: {
-    isActive: boolean;
-    type: itemTypes;
-    label: string;
-  }) => {
+  public _renderTab = (tab: TabData) => {
     const style = {
       borderColor: IconColors[tab.type],
       backgroundColor: BackgroundColors[tab.type],
@@ -66,7 +64,11 @@ export class NavigationFilter extends Component<FilterProps> {
         style={style}
         onClick={() => this._handleChange(tab.type)}>
         {tab.type === itemTypes.All ? (
-          <span>{this.props.translates[itemTypes.All]}</span>
+          <span>
+            <Text id={'navigation.All'}>
+              {this.props.defaultTranslates[itemTypes.All]}
+            </Text>
+          </span>
         ) : (
           <Fragment>
             <Tooltip label={tab.label}>
@@ -76,7 +78,9 @@ export class NavigationFilter extends Component<FilterProps> {
               />
             </Tooltip>
             {this.props.availableTabs.length < 4 && (
-              <span className={styles.label}>{tab.label}</span>
+              <span className={styles.label}>
+                <Text id={tab.i18nLabel}>{tab.label}</Text>
+              </span>
             )}
           </Fragment>
         )}
@@ -85,38 +89,54 @@ export class NavigationFilter extends Component<FilterProps> {
   };
 
   private _getTabsData = (): TabData[] => {
-    const {availableTabs, activeTab, translates} = this.props;
+    const {availableTabs, activeTab, defaultTranslates} = this.props;
     const tabs: TabData[] = availableTabs.map((tab: itemTypes) => {
       return {
         type: tab,
         isActive: activeTab === tab,
-        label: translates[tab],
+        label: defaultTranslates[tab],
+        i18nLabel: `navigation.${tab}`,
       };
     });
     return tabs;
   };
 
-  private _getResultLabel = (): string => {
-    const {activeTab, translates, totalResults} = this.props;
-    // TODO: add locale (i18n)
-    // TODO: look how player translates plural and single
-    // @ts-ignore
-    return `${totalResults} result${totalResults > 1 ? 's' : ''} in ${
+  private _getResultLabel = (
+    activeTab: itemTypes,
+    defaultTranslates: Record<string, string>,
+    totalResults: number
+  ) => {
+    const defaultTranslate = `${totalResults} result${
+      // @ts-ignore
+      totalResults > 1 ? 's' : ''
+    } in ${
       activeTab === itemTypes.All
         ? 'all content'
-        : translates[activeTab].toLowerCase()
+        : defaultTranslates[activeTab].toLowerCase()
     }`;
+    return (
+      // @ts-ignore
+      <Text
+        key={`result_${totalResults}`}
+        id="navigation.result"
+        plural={totalResults}
+        fields={{
+          count: totalResults,
+          tabName:
+            activeTab === itemTypes.All
+              ? 'all content'
+              : defaultTranslates[activeTab].toLowerCase(),
+        }}>
+        {defaultTranslate}
+      </Text>
+    );
   };
 
-  render() {
-    const {totalResults} = this.props;
+  render({activeTab, defaultTranslates, totalResults}: FilterProps) {
     const tabs = this._getTabsData();
-    if (tabs.length < 2) {
-      return null;
-    }
     return (
       <div className={styles.filterRoot}>
-        {totalResults !== 0 && (
+        {totalResults !== 0 && tabs.length > 1 && (
           <div className={styles.tabsWrapper}>
             {tabs.map(tab => {
               return this._renderTab(tab);
@@ -124,7 +144,9 @@ export class NavigationFilter extends Component<FilterProps> {
           </div>
         )}
         {!!totalResults && (
-          <div className={styles.totalResults}>{this._getResultLabel()}</div>
+          <div className={styles.totalResults}>
+            {this._getResultLabel(activeTab, defaultTranslates, totalResults)}
+          </div>
         )}
       </div>
     );
