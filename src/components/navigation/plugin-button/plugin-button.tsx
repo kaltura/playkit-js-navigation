388e3554ecd9bc8bf83aba5744a66de1a69fa080
Serhii <serhii.marchenko@Kaltura.com>
2021-04-05 15:25:44 +0300

    FEV-796 - Player V7| |Navigation 2.1.0| - UI - Icons on the upper menu are not as designed (#163)
    * fix(fev-796): plugin icon style
    
    * fix(fev-796): address PR comments

--- src/components/navigation/plugin-button/plugin-button.tsx
+++ src/components/navigation/plugin-button/plugin-button.tsx
@@ -3,11 +3,22 @@ import * as styles from './plugin-button.scss';
 
 interface PluginButtonProps {
   onClick: (event: MouseEvent) => void;
+  selected: boolean;
+  disabled?: boolean;
 }
 
-export const PluginButton = ({onClick}: PluginButtonProps) => {
+export const PluginButton = ({
+  onClick,
+  selected,
+  disabled,
+}: PluginButtonProps) => {
+  const iconStyles = [
+    styles.navigationPluginButton,
+    selected ? styles.selected : '',
+    disabled ? styles.disabled : '',
+  ].join(' ');
   return (
-    <button className={styles.navigationPluginButton} tabIndex={1} onClick={onClick}>
+    <button className={iconStyles} tabIndex={1} onClick={onClick}>
       <div className={styles.navigationPluginIcon} />
     </button>
   );
