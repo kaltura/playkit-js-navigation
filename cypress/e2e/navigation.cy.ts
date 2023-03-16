be6b02f463d8b1806a7ed1d6f53f780a23424d8f
Serhii Marchenko <serhii.marchenko@kaltura.com>
2023-03-16 18:36:32 +0200

    fix(FEC-12964): upd tests

--- cypress/e2e/navigation.cy.ts
+++ cypress/e2e/navigation.cy.ts
@@ -10,7 +10,7 @@ ${location.origin}/media/index.m3u8`;
 
 const preparePage = (navigationConf = {}, playbackConf = {}) => {
   cy.visit('index.html');
-  cy.window().then(win => {
+  return cy.window().then(win => {
     try {
       // @ts-ignore
       var kalturaPlayer = win.KalturaPlayer.setup({
@@ -29,7 +29,7 @@ const preparePage = (navigationConf = {}, playbackConf = {}) => {
         },
         playback: {muted: true, autoplay: true, ...playbackConf}
       });
-      kalturaPlayer.loadMedia({entryId: '0_wifqaipd'});
+      return kalturaPlayer.loadMedia({entryId: '0_wifqaipd'});
     } catch (e: any) {
       console.error(e.message);
     }
@@ -83,147 +83,158 @@ describe('Navigation plugin', () => {
   describe('plugin button and panel', () => {
     it('should open then close the navigation side panel', () => {
       mockKalturaBe();
-      preparePage();
-      clickNavigationPluginButton();
-      cy.get('[data-testid="navigation_root"]').should('exist');
-      cy.get('[data-testid="navigation_root"]').should('have.css', 'visibility', 'visible');
-      clickClosePluginButton();
-      cy.get('[data-testid="navigation_root"]').should('have.css', 'visibility', 'hidden');
+      preparePage().then(() => {
+        clickNavigationPluginButton();
+        cy.get('[data-testid="navigation_root"]').should('exist');
+        cy.get('[data-testid="navigation_root"]').should('have.css', 'visibility', 'visible');
+        clickClosePluginButton();
+        cy.get('[data-testid="navigation_root"]').should('have.css', 'visibility', 'hidden');
+      });
     });
 
     it('should open the navigation side panel if expandOnFirstPlay configuration is true', () => {
       mockKalturaBe();
-      preparePage({expandOnFirstPlay: true}, {muted: true, autoplay: true});
-      cy.get('[data-testid="navigation_pluginButton"]').should('exist');
-      cy.get('[data-testid="navigation_root"]').should('have.css', 'visibility', 'visible');
+      preparePage({expandOnFirstPlay: true}, {muted: true, autoplay: true}).then(() => {
+        cy.get('[data-testid="navigation_pluginButton"]').should('exist');
+        cy.get('[data-testid="navigation_root"]').should('have.css', 'visibility', 'visible');
+      });
     });
 
     it('should close plugin if ESC button pressed', () => {
       mockKalturaBe();
-      preparePage({expandOnFirstPlay: true}, {muted: true, autoplay: true});
-      cy.get('[data-testid="navigation_root"]').should('have.css', 'visibility', 'visible');
-      cy.get("[aria-label='Search in video']").type('{esc}');
-      cy.get('[data-testid="navigation_root"]').should('have.css', 'visibility', 'hidden');
-    });
-  });
-
-  describe('navigation data', () => {
-    it('should test fetch of slides and chapters image', () => {
-      mockKalturaBe();
-      preparePage({expandOnFirstPlay: true}, {muted: true, autoplay: true});
-      cy.get('@getChapters.all').should('have.length', 3);
-      cy.get('@getSlides.all').should('have.length', 2);
-    });
-
-    it('should render navigation items', () => {
-      mockKalturaBe();
-      preparePage({expandOnFirstPlay: true}, {muted: true, autoplay: true});
-      cy.get('[data-testid="navigation_root"]').within(() => {
-        cy.get('[data-testid="navigation_list"]').children().should('have.length', 6);
+      preparePage({expandOnFirstPlay: true}, {muted: true, autoplay: true}).then(() => {
+        cy.get('[data-testid="navigation_root"]').should('have.css', 'visibility', 'visible');
+        cy.get("[aria-label='Search in video']").type('{esc}');
+        cy.get('[data-testid="navigation_root"]').should('have.css', 'visibility', 'hidden');
       });
     });
 
-    it('should handle click and make seek to cue startTime', () => {
-      mockKalturaBe();
-      preparePage({expandOnFirstPlay: true}, {muted: true, autoplay: true});
-      cy.get('[data-testid="navigation_root"]').within(() => {
-        const chapterItem = cy.get('[aria-label="chapter 2"]').should('have.attr', 'aria-current', 'false');
-        chapterItem.click({force: true});
-        chapterItem.should('have.attr', 'aria-current', 'true');
+    describe('navigation data', () => {
+      it('should test fetch of slides and chapters image', () => {
+        mockKalturaBe();
+        preparePage({expandOnFirstPlay: true}, {muted: true, autoplay: true}).then(() => {
+          cy.get('@getChapters.all').should('have.length', 3);
+          cy.get('@getSlides.all').should('have.length', 2);
+        });
       });
-    });
 
-    it('should highlight navigation item when playback reach cue startTime', () => {
-      mockKalturaBe();
-      preparePage({expandOnFirstPlay: true}, {muted: true, autoplay: true});
-      cy.window().then($win => {
-        // @ts-ignore
-        const kalturaPlayer = $win.KalturaPlayer.getPlayers()['player-placeholder'];
-        cy.get('[data-testid="navigation_root"]').within(() => {
-          cy.get('[aria-label="Hotspot 1"]').should('have.attr', 'aria-current', 'false');
-          kalturaPlayer.currentTime = 12;
-          cy.get('[aria-label="Hotspot 1"]').should('have.attr', 'aria-current', 'true');
+      it('should render navigation items', () => {
+        mockKalturaBe();
+        preparePage({expandOnFirstPlay: true}, {muted: true, autoplay: true}).then(() => {
+          cy.get('[data-testid="navigation_root"]').within(() => {
+            cy.get('[data-testid="navigation_list"]').children().should('have.length', 6);
+          });
+        });
+      });
+
+      it('should handle click and make seek to cue startTime', () => {
+        mockKalturaBe();
+        preparePage({expandOnFirstPlay: true}, {muted: true, autoplay: true}).then(() => {
+          cy.get('[data-testid="navigation_root"]').within(() => {
+            const chapterItem = cy.get('[aria-label="chapter 2"]').should('have.attr', 'aria-current', 'false');
+            chapterItem.click({force: true});
+            chapterItem.should('have.attr', 'aria-current', 'true');
+          });
+        });
+      });
+
+      it('should highlight navigation item when playback reach cue startTime', () => {
+        mockKalturaBe();
+        preparePage({expandOnFirstPlay: true}, {muted: true, autoplay: true}).then(() => {
+          cy.window().then($win => {
+            // @ts-ignore
+            const kalturaPlayer = $win.KalturaPlayer.getPlayers()['player-placeholder'];
+            cy.get('[data-testid="navigation_root"]').within(() => {
+              cy.get('[aria-label="Hotspot 1"]').should('have.attr', 'aria-current', 'false');
+              kalturaPlayer.currentTime = 12;
+              cy.get('[aria-label="Hotspot 1"]').should('have.attr', 'aria-current', 'true');
+            });
+          });
+        });
+      });
+
+      it('should highlight different type of navigation items according to playback ', () => {
+        mockKalturaBe();
+        preparePage({expandOnFirstPlay: true}, {muted: true, autoplay: true}).then(() => {
+          cy.window().then($win => {
+            // @ts-ignore
+            const kalturaPlayer = $win.KalturaPlayer.getPlayers()['player-placeholder'];
+            cy.get('[data-testid="navigation_root"]').within(() => {
+              cy.get('[data-entry-id="1_02sihd5j"]').should('have.attr', 'aria-current', 'false');
+              cy.get("[aria-label='Search in video']").type('c');
+              kalturaPlayer.currentTime = 12;
+              cy.get('[data-entry-id="1_nkiuwh50-1"]').should('have.attr', 'aria-current', 'true');
+              cy.get("[aria-label='List Chapters']").click();
+              cy.get('[data-entry-id="1_02sihd5j"]').should('have.attr', 'aria-current', 'true');
+            });
+          });
         });
       });
     });
 
-    it('should highlight different type of navigation items according to playback ', () => {
-      mockKalturaBe();
-      preparePage({expandOnFirstPlay: true}, {muted: true, autoplay: true});
-      cy.window().then($win => {
-        // @ts-ignore
-        const kalturaPlayer = $win.KalturaPlayer.getPlayers()['player-placeholder'];
-        cy.get('[data-testid="navigation_root"]').within(() => {
-          cy.get('[data-entry-id="1_02sihd5j"]').should('have.attr', 'aria-current', 'false');
-          cy.get("[aria-label='Search in video']").type('c');
-          kalturaPlayer.currentTime = 12;
-          cy.get('[data-entry-id="1_nkiuwh50-1"]').should('have.attr', 'aria-current', 'true');
-          cy.get("[aria-label='List Chapters']").click();
-          cy.get('[data-entry-id="1_02sihd5j"]').should('have.attr', 'aria-current', 'true');
+    describe('search and filter', () => {
+      it('should set focus to search input if plugin opened by keyboard', () => {
+        mockKalturaBe();
+        preparePage().then(() => {
+          cy.get('[data-testid="navigation_pluginButton"]').should('exist').trigger('keydown', {
+            keyCode: 32, // Space
+            force: true
+          });
+          cy.get("[aria-label='Search in video']").should('have.focus');
         });
       });
-    });
-  });
-
-  describe('search and filter', () => {
-    it('should set focus to search input if plugin opened by keyboard', () => {
-      mockKalturaBe();
-      preparePage();
-      cy.get('[data-testid="navigation_pluginButton"]').should('exist').trigger('keydown', {
-        keyCode: 32, // Space
-        force: true
+      it('should test search bar', () => {
+        mockKalturaBe();
+        preparePage({expandOnFirstPlay: true}, {muted: true, autoplay: true}).then(() => {
+          cy.get('[data-testid="navigation_root"]').within(() => {
+            const searchInput = cy.get("[aria-label='Search in video']");
+            searchInput.should('be.visible');
+            searchInput.type('1');
+            searchInput.should('have.value', '1');
+            cy.get('[data-testid="navigation_list"]').children().should('have.length', 1);
+            const clearSearchButton = cy.get("[aria-label='Clear search']");
+            clearSearchButton.should('be.visible');
+            clearSearchButton.click();
+            searchInput.should('have.value', '');
+            cy.get('[data-testid="navigation_list"]').children().should('have.length', 6);
+          });
+        });
       });
-      cy.get("[aria-label='Search in video']").should('have.focus');
-    });
-    it('should test search bar', () => {
-      mockKalturaBe();
-      preparePage({expandOnFirstPlay: true}, {muted: true, autoplay: true});
-      cy.get('[data-testid="navigation_root"]').within(() => {
-        const searchInput = cy.get("[aria-label='Search in video']");
-        searchInput.should('be.visible');
-        searchInput.type('1');
-        searchInput.should('have.value', '1');
-        cy.get('[data-testid="navigation_list"]').children().should('have.length', 1);
-        const clearSearchButton = cy.get("[aria-label='Clear search']");
-        clearSearchButton.should('be.visible');
-        clearSearchButton.click();
-        searchInput.should('have.value', '');
-        cy.get('[data-testid="navigation_list"]').children().should('have.length', 6);
-      });
-    });
-    it('should test filter tabs', () => {
-      mockKalturaBe();
-      preparePage({expandOnFirstPlay: true}, {muted: true, autoplay: true});
-      cy.get('[data-testid="navigation_root"]').within(() => {
-        // default state
-        const tabAll = cy.get("[aria-label='List All']").should('be.visible').should('have.attr', 'aria-checked', 'true');
-        const tabChapters = cy.get("[aria-label='List Chapters']").should('be.visible').should('have.attr', 'aria-checked', 'false');
-        const tabSlides = cy.get("[aria-label='List Slides']").should('be.visible').should('have.attr', 'aria-checked', 'false');
-        const tabHotspots = cy.get("[aria-label='List Hotspots']").should('be.visible').should('have.attr', 'aria-checked', 'false');
-        cy.get("[aria-label='Captions']").should('not.exist');
-        cy.get("[aria-label='AoA']").should('not.exist');
+      it('should test filter tabs', () => {
+        mockKalturaBe();
+        preparePage({expandOnFirstPlay: true}, {muted: true, autoplay: true}).then(() => {
+          cy.get('[data-testid="navigation_root"]').within(() => {
+            // default state
+            const tabAll = cy.get("[aria-label='List All']").should('be.visible').should('have.attr', 'aria-checked', 'true');
+            const tabChapters = cy.get("[aria-label='List Chapters']").should('be.visible').should('have.attr', 'aria-checked', 'false');
+            const tabSlides = cy.get("[aria-label='List Slides']").should('be.visible').should('have.attr', 'aria-checked', 'false');
+            const tabHotspots = cy.get("[aria-label='List Hotspots']").should('be.visible').should('have.attr', 'aria-checked', 'false');
+            cy.get("[aria-label='Captions']").should('not.exist');
+            cy.get("[aria-label='AoA']").should('not.exist');
 
-        // apply filters
-        tabChapters.click();
-        cy.get('[data-testid="navigation_list"]').children().should('have.length', 3);
-        tabSlides.click();
-        cy.get('[data-testid="navigation_list"]').children().should('have.length', 2);
-        tabHotspots.click();
-        cy.get('[data-testid="navigation_list"]').children().should('have.length', 1);
-        tabAll.click();
-        cy.get('[data-testid="navigation_list"]').children().should('have.length', 6);
+            // apply filters
+            tabChapters.click();
+            cy.get('[data-testid="navigation_list"]').children().should('have.length', 3);
+            tabSlides.click();
+            cy.get('[data-testid="navigation_list"]').children().should('have.length', 2);
+            tabHotspots.click();
+            cy.get('[data-testid="navigation_list"]').children().should('have.length', 1);
+            tabAll.click();
+            cy.get('[data-testid="navigation_list"]').children().should('have.length', 6);
 
-        // apply search
-        cy.get("[aria-label='Captions']").should('not.exist');
-        cy.get("[aria-label='Search in video']").type('c');
-        cy.get("[aria-label='List All']").should('be.visible');
-        cy.get("[aria-label='List Chapters']").should('be.visible');
-        cy.get("[aria-label='Slides']").should('not.exist');
-        cy.get("[aria-label='Hotspots']").should('not.exist');
-        cy.get("[aria-label='AoA']").should('not.exist');
-        const tabCaptions = cy.get("[aria-label='List Captions']").should('be.visible').click();
-        tabCaptions.should('have.attr', 'aria-checked', 'true');
-        cy.get('[data-testid="navigation_list"]').children().should('have.length', 5);
+            // apply search
+            cy.get("[aria-label='Captions']").should('not.exist');
+            cy.get("[aria-label='Search in video']").type('c');
+            cy.get("[aria-label='List All']").should('be.visible');
+            cy.get("[aria-label='List Chapters']").should('be.visible');
+            cy.get("[aria-label='Slides']").should('not.exist');
+            cy.get("[aria-label='Hotspots']").should('not.exist');
+            cy.get("[aria-label='AoA']").should('not.exist');
+            const tabCaptions = cy.get("[aria-label='List Captions']").should('be.visible').click();
+            tabCaptions.should('have.attr', 'aria-checked', 'true');
+            cy.get('[data-testid="navigation_list"]').children().should('have.length', 5);
+          });
+        });
       });
     });
   });
