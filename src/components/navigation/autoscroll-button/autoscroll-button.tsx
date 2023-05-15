0ce2d6f78c5113fbf390f9e034883c27d0eaa50a
Serhii <serhii.marchenko@Kaltura.com>
2023-05-15 11:11:17 +0300

    fix(FEC-13145): upgrade dependancies for common and ui-managers etc (#317)
    * fix(FEC-13145): upd dependencies
    
    * fix(FEC-13145): upd dependencies

--- src/components/navigation/autoscroll-button/autoscroll-button.tsx
+++ src/components/navigation/autoscroll-button/autoscroll-button.tsx
@@ -1,5 +1,5 @@
 import {h} from 'preact';
-import {ui} from 'kaltura-player-js';
+import {ui} from '@playkit-js/kaltura-player-js';
 import * as styles from './autoscroll-button.scss';
 import {A11yWrapper, OnClick} from '@playkit-js/common/dist/hoc/a11y-wrapper';
 
