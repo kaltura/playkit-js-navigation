5774fc8b497cae425225451222fc8ea8ea5f237f
Tzipi <101048005+Tzipi-kaltura@users.noreply.github.com>
2025-12-08 11:18:36 +0200

    fix(FEC:14736): Remove access to window.KalturaPlayer from plugns (#396)
    use registerPlugin function from kaltura-player-js and not from window.KalturaPlayer
    
    solves FEC-14736

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { NavigationPlugin, pluginName } from "./navigation-plugin";
+import {registerPlugin} from '@playkit-js/kaltura-player-js';
 
 declare var __VERSION__: string;
 declare var __NAME__: string;
@@ -10,4 +11,4 @@ export {NavigationPlugin as Plugin};
 export {VERSION, NAME};
 export {NavigationEvent} from './events/events'
 
-KalturaPlayer.core.registerPlugin(pluginName, NavigationPlugin);
+registerPlugin(pluginName, NavigationPlugin as any);
