2106ccd483fec3709461c6cf6b438975cd2188d5
Valentin Stoica <162963925+valentinStoicaK@users.noreply.github.com>
2026-06-08 14:05:58 +0300

    fix(ADA-3331): Fix Navigation item invalid structure (#399)
    This fixes https://kaltura.atlassian.net/browse/ADA-3331.
    Issue description: The More/Less button from the ExpandableText is
    rendered inside the NavigationItem that is also a button. This creates
    an invalid structure.
    Changes description: This was redesigned
    https://www.figma.com/design/8yTuCnuVHLYTFp89w7z05J/%F0%9F%93%BA-Player-v7?node-id=539-88&p=f&m=dev,
    with a button that is rendered outside of the NavigationItem. This
    button expands and collapses the description. When this action is
    performed, the SR will announce the expanded state and also the content
    of this NavigationItem using aria-live.
    
    Note: The "Toggle description" text from the aria-label has not been
    discussed with product. It is a suggestion and will be changed in this
    ticket https://kaltura.atlassian.net/browse/ADA-3332 when Julien
    returns. It was added here so this won't generate new tickets for
    missing accessible name

--- src/components/navigation/navigation-item/NavigationItem.tsx
+++ src/components/navigation/navigation-item/NavigationItem.tsx
@@ -1,5 +1,6 @@
 import {Component, h, Fragment} from 'preact';
 import {A11yWrapper} from '@playkit-js/common/dist/hoc/a11y-wrapper';
+import {ChevronRight} from '@playkit-js/common/dist/icon/icons/chevronRight';
 import * as styles from './NavigationItem.scss';
 import {GroupTypes, ItemData} from '../../../types';
 import {IconsFactory} from '../icons/IconsFactory';
@@ -9,8 +10,7 @@ const {preacti18n} = ui;
 
 //@ts-ignore
 const {getDurationAsText} = KalturaPlayer.ui.utils;
-const {ExpandableText} = ui.components;
-const {withText, Text, Localizer} = preacti18n;
+const {withText, Text} = preacti18n;
 const {withPlayer} = KalturaPlayer.ui.components;
 
 export interface NavigationItemProps {
@@ -27,6 +27,7 @@ export interface NavigationItemProps {
   slideAltText?: string;
   instructionLabel?: string;
   timeLabel?: string;
+  toggleLabel?: string;
   player?: any;
 }
 
@@ -34,11 +35,15 @@ export interface NavigationItemState {
   imageLoaded: boolean;
   imageFailed: boolean;
   useExpandableText: boolean;
+  isExpanded: boolean;
+  announcement: string;
+  hasOverflow: boolean;
 }
 const translates={
   slideAltText: <Text id="navigation.slide_type.one">Slide</Text>,
   instructionLabel: <Text id="navigation.instruction_label">Jump to this point in video</Text>,
   timeLabel: <Text id="navigation.time_label">Timestamp</Text>,
+  toggleLabel: <Text id="navigation.toggle_description">Toggle description</Text>,
 
 };
 function getAriaLabelTitle(data: ItemData): string {
@@ -55,13 +60,19 @@ function getAriaLabelTitle(data: ItemData): string {
 export class NavigationItem extends Component<NavigationItemProps, NavigationItemState> {
   private _itemElementRef: HTMLDivElement | null = null;
   private _textContainerRef: HTMLDivElement | null = null;
+  private _descriptionRef: HTMLDivElement | null = null;
+  private _announcementTimeout?: number;
+  private _overflowCheckFrame?: number;
 
   constructor(props: NavigationItemProps) {
     super(props);
     this.state = {
       imageLoaded: false,
       imageFailed: false,
-      useExpandableText: typeof this.props.data?.displayTitle === 'string'
+      useExpandableText: typeof this.props.data?.displayTitle === 'string',
+      isExpanded: false,
+      announcement: '',
+      hasOverflow: false
     };
   }
 
@@ -84,6 +95,10 @@ export class NavigationItem extends Component<NavigationItemProps, NavigationIte
       data !== nextProps.data ||
       nextState.imageLoaded !== this.state.imageLoaded ||
       nextState.imageFailed !== this.state.imageFailed ||
+      nextState.isExpanded !== this.state.isExpanded ||
+      nextState.announcement !== this.state.announcement ||
+      nextState.hasOverflow !== this.state.hasOverflow ||
+      nextState.useExpandableText !== this.state.useExpandableText ||
       nextProps.widgetWidth !== widgetWidth
     ) {
       return true;
@@ -91,16 +106,43 @@ export class NavigationItem extends Component<NavigationItemProps, NavigationIte
     return false;
   }
 
-  componentDidUpdate(previousProps: Readonly<NavigationItemProps>, nextState: Readonly<NavigationItemState>) {
+  componentDidUpdate(previousProps: Readonly<NavigationItemProps>, previousState: Readonly<NavigationItemState>) {
+    if (this.state.announcement) {
+      window.clearTimeout(this._announcementTimeout);
+
+      this._announcementTimeout = window.setTimeout(() => {
+        this.setState({announcement: ''});
+      }, 150);
+    }
+
     this._getSelected();
     this.matchHeight();
+    
+    // Only check overflow when conditions that affect it change
+    const relevantPropsChanged = 
+      previousProps.widgetWidth !== this.props.widgetWidth ||
+      previousProps.data.displayDescription !== this.props.data.displayDescription;
+    const imageJustLoaded = !previousState.imageLoaded && this.state.imageLoaded;
+    const justCollapsed = previousState.isExpanded && !this.state.isExpanded;
+    
+    if (relevantPropsChanged || imageJustLoaded || justCollapsed) {
+      this._checkOverflow();
+    }
   }
 
   componentDidMount() {
     this._getSelected();
     this.matchHeight();
+    this._checkOverflow();
   }
-
+  componentWillUnmount() {
+    if (this._announcementTimeout) {
+      window.clearTimeout(this._announcementTimeout);
+    }
+    if (this._overflowCheckFrame) {
+      window.cancelAnimationFrame(this._overflowCheckFrame);
+    }
+  }
   private _getSelected = () => {
     const {selectedItem, data} = this.props;
     const {groupData, startTime, previewImage} = data;
@@ -123,12 +165,83 @@ export class NavigationItem extends Component<NavigationItemProps, NavigationIte
     this.props.onClick(this.props.data.startTime, this.props.data.itemType);
   };
 
-  private _onExpand = (isTextExpanded: boolean) => {
-    this.props.dispatcher(NavigationEvent.NAVIGATION_EXPANDABLE_TEXT_CLICK, {isTextExpanded, itemType: this.props.data.itemType});
-  };
-  private _handleExpand = (e: MouseEvent) => {
+  private _toggleExpand = (e: MouseEvent | KeyboardEvent) => {
     e?.stopPropagation();
     e?.preventDefault();
+
+    this.setState((prevState) => {
+      const newExpandedState = !prevState.isExpanded;
+
+      this.props.dispatcher(NavigationEvent.NAVIGATION_EXPANDABLE_TEXT_CLICK, {
+        isTextExpanded: newExpandedState,
+        itemType: this.props.data.itemType
+      });
+
+      let announcement = '';
+
+      if (newExpandedState) {
+        const {displayDescription, startTime} = this.props.data;
+        const timestamp = getDurationAsText(Math.floor(startTime), this.props.player?.config.ui.locale, true);
+        const titleText = getAriaLabelTitle(this.props.data);
+        const descriptionText = typeof displayDescription === 'string' ? displayDescription : '';
+        announcement = [`${this.props.timeLabel} ${timestamp}`, titleText, descriptionText].filter(Boolean).join('. ');
+      }
+
+      return {
+        isExpanded: newExpandedState,
+        announcement
+      };
+    });
+  };
+
+  private _checkOverflow = () => {
+    // Cancel any pending check
+    if (this._overflowCheckFrame) {
+      window.cancelAnimationFrame(this._overflowCheckFrame);
+    }
+    
+    // Schedule measurement in RAF to batch layout reads and reduce thrashing
+    this._overflowCheckFrame = window.requestAnimationFrame(() => {
+      // Only check overflow when collapsed - when expanded, the content is not clamped
+      if (this.state.isExpanded) {
+        return;
+      }
+      
+      if (!this._descriptionRef || !this.props.data.displayDescription) {
+        if (this.state.hasOverflow) {
+          this.setState({hasOverflow: false});
+        }
+        return;
+      }
+
+      const isOverflowing = this._descriptionRef.scrollHeight > this._descriptionRef.clientHeight;
+      if (isOverflowing !== this.state.hasOverflow) {
+        // Reset isExpanded when overflow disappears to keep state clean
+        const shouldResetExpanded = !isOverflowing && this.state.isExpanded;
+        this.setState({
+          hasOverflow: isOverflowing,
+          ...(shouldResetExpanded ? {isExpanded: false} : {})
+        });
+      }
+    });
+  };
+
+  private _renderToggleButton = () => {
+    if (!this.state.hasOverflow) {
+      return null;
+    }
+
+    return (
+    <button
+      onClick={this._toggleExpand}
+      className={[styles.toggleButton, this.state.isExpanded ? styles.toggleButtonExpanded : null].filter(Boolean).join(' ')}
+      aria-expanded={this.state.isExpanded}
+      aria-controls={`nav-content-${this.props.data.id}`}
+      type="button"
+      aria-label={this.props.toggleLabel}>
+      <ChevronRight />
+    </button>
+    );
   };
 
   private _renderThumbnail = () => {
@@ -152,28 +265,24 @@ export class NavigationItem extends Component<NavigationItemProps, NavigationIte
     return <img {...imageProps} />;
   };
 
-  private _renderTitleAndDescription = (ariaLabelTitle: string) => {
+  private _renderTitleAndDescription = () => {
     const {previewImage, displayTitle, displayDescription} = this.props.data;
+    const {isExpanded} = this.state;
     const hasTitle = Boolean(displayTitle || displayDescription);
     if (previewImage && hasTitle) {
       if (this.state.useExpandableText) {
         return (
           <div className={styles.titleWrapper}>
-            <ExpandableText
-              text={ariaLabelTitle || displayDescription || ''}
-              lines={1}
-              forceShowMore={Boolean(displayTitle && displayDescription)}
-              onClick={this._handleExpand}
-              onExpand={this._onExpand}
-              className={styles.expandableText}
-              classNameExpanded={styles.expanded}
-              buttonProps={{
-                tabIndex: 0,
-                role: 'button'
-              }}>
+            <div id={`nav-content-${this.props.data.id}`}>
               {displayTitle && <div className={styles.title}>{displayTitle}</div>}
-              {displayDescription && <div className={styles.descriptionWrapper}>{displayDescription}</div>}
-            </ExpandableText>
+              {displayDescription && (
+                <div
+                  ref={node => { this._descriptionRef = node; }}
+                  className={[styles.descriptionWrapper, !isExpanded ? styles['clamped-1'] : null].filter(Boolean).join(' ')}>
+                  {displayDescription}
+                </div>
+              )}
+            </div>
           </div>
         );
       }
@@ -184,22 +293,12 @@ export class NavigationItem extends Component<NavigationItemProps, NavigationIte
         {displayTitle && <div className={styles.title}>{displayTitle}</div>}
         {displayDescription && (
           <div className={styles.descriptionWrapper}>
-            <Localizer>
-              <ExpandableText
-                buttonProps={{
-                  tabIndex: 0,
-                  readMoreLabel: <Text id="navigation.read_more">Read more</Text>,
-                  readLessLabel: <Text id="navigation.read_less">Read less</Text>
-                }}
-                text={displayDescription}
-                lines={3}
-                className={styles.expandableText}
-                classNameExpanded={styles.expanded}
-                onClick={this._handleExpand}
-                onExpand={this._onExpand}>
-                {displayDescription}
-              </ExpandableText>
-            </Localizer>
+            <div
+              ref={node => { this._descriptionRef = node; }}
+              className={[isExpanded ? styles.expanded : styles.expandableText, !isExpanded ? styles['clamped-3'] : null].filter(Boolean).join(' ')}
+              id={`nav-content-${this.props.data.id}`}>
+              {displayDescription}
+            </div>
           </div>
         )}
       </Fragment>
@@ -208,46 +307,67 @@ export class NavigationItem extends Component<NavigationItemProps, NavigationIte
 
   render() {
     const {data, selectedItem, showIcon, instructionLabel, timeLabel, player} = this.props;
-    const {id, previewImage, itemType, displayTime, liveCuePoint, groupData, displayTitle, displayDescription, startTime} = data;
-    const {imageLoaded} = this.state;
+    const {id, previewImage, itemType, displayTime, liveCuePoint, groupData, displayDescription, startTime} = data;
+    const {imageLoaded, isExpanded, hasOverflow} = this.state;
     const ariaLabelTitle = getAriaLabelTitle(data);
     const timestampLabel = `${timeLabel} ${getDurationAsText(Math.floor(startTime), player?.config.ui.locale, true)}`
+    
+    // Include description in aria-label when it is not overflowing
+    const descriptionText = typeof displayDescription === 'string' ? displayDescription : '';
+    const includeDescriptionInLabel = descriptionText && !hasOverflow;
+    const ariaLabelParts = [timestampLabel, ariaLabelTitle];
+    if (includeDescriptionInLabel) {
+      ariaLabelParts.push(descriptionText);
+    }
+    if (instructionLabel) {
+      ariaLabelParts.push(instructionLabel);
+    }
 
     const a11yProps: Record<string, any> = {
       ['aria-current']: selectedItem,
-      ['aria-label']: timestampLabel + " " + ariaLabelTitle + " " + instructionLabel,
+      ['aria-label']: ariaLabelParts.join(' '),
+      ['aria-describedby']: isExpanded && displayDescription && hasOverflow ? `nav-content-${id}` : undefined,
       tabIndex: 0,
       role: 'button'
     };
 
     return (
-      <A11yWrapper onClick={this._handleClick}>
+      <div className={styles.itemContainer} data-testid="nav-item-container">
+        <A11yWrapper onClick={this._handleClick}>
+          <div
+            ref={node => {
+              this._itemElementRef = node;
+            }}
+            className={[
+              styles[groupData ? groupData : 'single'],
+              styles.navigationItem,
+              selectedItem ? styles.selected : null,
+              previewImage && !imageLoaded ? styles.hidden : null
+            ].filter(Boolean).join(' ')}
+            data-entry-id={id}
+            {...a11yProps}>
+            <div className={[styles.metadata, liveCuePoint ? null : styles.withTime].filter(Boolean).join(' ')}>
+              {!liveCuePoint && <span>{displayTime}</span>}
+              {showIcon && (
+                <div className={styles.iconWrapper}>
+                  <IconsFactory iconType={itemType} />
+                </div>
+              )}
+            </div>
+            <div className={[styles.content, previewImage ? styles.hasImage : null].filter(Boolean).join(' ')}>
+              {this._renderTitleAndDescription()}
+              {previewImage && this._renderThumbnail()}
+            </div>
+          </div>
+        </A11yWrapper>
+        {this._renderToggleButton()}
         <div
-          ref={node => {
-            this._itemElementRef = node;
-          }}
-          className={[
-            styles[groupData ? groupData : 'single'],
-            styles.navigationItem,
-            selectedItem ? styles.selected : null,
-            previewImage && !imageLoaded ? styles.hidden : null
-          ].join(' ')}
-          data-entry-id={id}
-          {...a11yProps}>
-          <div className={[styles.metadata, liveCuePoint ? null : styles.withTime].join(' ')}>
-            {!liveCuePoint && <span>{displayTime}</span>}
-            {showIcon && (
-              <div className={styles.iconWrapper}>
-                <IconsFactory iconType={itemType} />
-              </div>
-            )}
-          </div>
-          <div className={[styles.content, previewImage ? styles.hasImage : null].join(' ')}>
-            {this._renderTitleAndDescription(ariaLabelTitle)}
-            {previewImage && this._renderThumbnail()}
-          </div>
+          aria-live="polite"
+          aria-atomic="true"
+          className={styles.srOnly}>
+          {this.state.announcement}
         </div>
-      </A11yWrapper>
+      </div>
     );
   }
 }
