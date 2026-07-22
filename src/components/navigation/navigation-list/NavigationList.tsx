3773875c28eb4831822f18e50bfb6feedb34a6c6
valentinStoicaK <162963925+valentinStoicaK@users.noreply.github.com>
2026-07-22 14:30:42 +0300

    fix(ADA-2775): Fix list not scrolling to focused navigationItem

--- src/components/navigation/navigation-list/NavigationList.tsx
+++ src/components/navigation/navigation-list/NavigationList.tsx
@@ -59,6 +59,7 @@ export class NavigationList extends Component<Props> {
     const focusableElement = item.base.querySelector(`[data-entry-id="${itemId}"]`);
     if (focusableElement instanceof HTMLElement) {
       focusableElement.focus({preventScroll: true});
+      focusableElement.scrollIntoView({block: 'nearest', inline: 'nearest'});
     }
   };
 
